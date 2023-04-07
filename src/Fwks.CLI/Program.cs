37985e973730f52869bed30f5fc66eb18f0c68a0
Murilo Almeida <dev@muriloalmeida.dev>
2023-04-07 02:27:19 +0100

    added fwks cli initial tests.

---
+++ src/Fwks.CLI/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Net.Http;
+
+try
+{
+    using var httpClient = new HttpClient();
+
+    using var stream = await httpClient.GetStreamAsync(AppSettings.TemplateZipUri);
+
+    Directory.CreateDirectory(AppSettings.AppFolder);
+
+    using var fileStream = new FileStream(AppSettings.LatestTemplateFile, FileMode.OpenOrCreate);
+
+    await stream.CopyToAsync(fileStream);
+
+    var tempPath = AppSettings.AppFolder;
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+public static class AppSettings
+{
+    public static string AppFolder => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Fwks CLI");
+    public static string TemplateZipUri => "https://github.com/morilon/fwks/archive/refs/heads/main.zip";
+    public static string LatestTemplateFile => Path.Combine(AppFolder, "latest-template.zip");
+}
